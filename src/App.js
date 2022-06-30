72021c39f0ab7c8aa7e1416866b5d78b41bd4d60
Brett <bdmdiving@gmail.com>
2022-06-30 20:45:58 +1000

    setting things up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react'
 
 function App() {
+  
+  
+  
+  
+  
+  
+  
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Form onSubmit={handleSubmit}>
+    <Form.Group className="mb-3" >
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" value={message.name} onChange={handleChange} name="name" />
+    </Form.Group>
+    <Form.Group className="mb-3" >
+        <Form.Label>Message</Form.Label>
+        <Form.Control type="text" value={message.message} onChange={handleChange} name="message" />
+    </Form.Group>
+
+    <Button type="submit">Send</Button>
+</Form>
+  )
 }
 
 export default App;
